ac39e1e40ee66bab875e6da2accab0c7e27e3a51
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-02 15:54:15 +0000

    numberConversion

---
+++ 01_basic/04_numberConversion.js
@@ -0,0 +1,11 @@
+// let age = "33abe"
+// let age = null
+// let age = true
+// let age = false
+// console.log(typeof(age))
+// let changeNumber = Number(age)
+// console.log(changeNumber)
+
+let isLogged = "laxmi"
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged )
