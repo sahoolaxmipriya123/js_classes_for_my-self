dad63c80efb5a07ead52f776acdbfa9216d86d96
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-02 06:38:12 +0000

    container config done

--- 01_basic/02_class.js
+++ 01_basic/02_class.js
@@ -2,11 +2,12 @@ const accoundId = 123456
 let accountEmail = "sahoolaxmipriya533@gmail.com"
 var acountPassword = 15467
 acountCity = "bangalore"
+let accountState;
 // let accoundId = 456
 console.log(accoundId)
 accountEmail = "sahoolaxmi@gmail.com"
 acountPassword = 789
 acountCity = "jaipur"
-console.table([accoundId,accountEmail,acountCity,acountPassword])
+console.table([accoundId,accountEmail,acountCity,acountPassword,accountState])
 
 
