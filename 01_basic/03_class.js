4038dacbffbb516ce3eb2bb2c6fbb5bec2af9cb2
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-02 15:09:42 +0000

    container config done

--- 01_basic/03_class.js
+++ 01_basic/03_class.js
@@ -2,3 +2,21 @@
 
 
 //  alert("i am learning javascript") we are using node.js not browser.
+
+
+let name = "laxmipriya"
+let age = 45
+let isLogged = true
+let state = null
+
+/*
+type of data type in js
+1.number = 45
+2.bigint = 4567894
+3.string = "laxmipriya" =>string should be written in ""
+4.boolean = true of false
+5.null = standalone value 
+6.undefind = 
+7.symbol*/
+
+
