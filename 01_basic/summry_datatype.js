350c5b0fcf45524ffe6793b5995013bf05a72d6a
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-03 06:03:07 +0000

    summry

---
+++ 01_basic/summry_datatype.js
@@ -0,0 +1,23 @@
+// primitive
+
+// 7 type of data type : string , number,Boolean,null,undefind,symbol,Bigint
+  
+
+
+// reference (Non primitive)
+
+// Array, object,functions
+
+let flowers = ["rose","lily","dalia"]     //array
+
+let obj = {
+    name :"laxmipriya",   //object
+    age : 22,
+}
+
+let myFunction  = function(){    //function
+    console.log("Hello World")
+}
+
+
+// stack(primitive)  Heap(Non-primitive)
