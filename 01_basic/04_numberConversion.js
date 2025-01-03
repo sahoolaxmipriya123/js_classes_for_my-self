cd9b60a88920ce36a07c2d2618f221627bd21c4c
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-03 04:49:01 +0000

    conversion

--- 01_basic/04_numberConversion.js
+++ 01_basic/04_numberConversion.js
@@ -5,7 +5,52 @@
 // console.log(typeof(age))
 // let changeNumber = Number(age)
 // console.log(changeNumber)
+// "33"=>33
+// "33abc"=>NaN
 
 let isLogged = "laxmi"
 let booleanIsLogged = Boolean(isLogged)
-console.log(booleanIsLogged )
+// console.log(booleanIsLogged )
+// 1=>true, 0=> false;
+// "" =>false;
+// "laxmi"=>true;
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//  console.log(stringNumber)
+// console.log(typeof(stringNumber));
+
+// operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str = "laxmi"
+let str1 = " priya"
+let str2 = str+str1
+// console.log(str+str1)
+
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log(1+"2"+"2")
+// console.log("1"+2+3)
+// console.log(1+2+"3")
+
+
+// console.log(+true)
+// console.log(true+)
+let num1,num2,num3
+num1=num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+gameCounter--
+console.log(gameCounter)
