c980db8ab72abc44d31a907da1e4762ecc5978da
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-04 05:21:25 +0000

    date and time

--- 01_basic/07_num_math.js
+++ 01_basic/07_num_math.js
@@ -1,4 +1,41 @@
 const score = 100
-console.log(score)
+// console.log(score)
 const balance = new Number(400)
-console.log(balance)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+
+const otherNumber = 254.565  //it round the number accoding to user input.
+// console.log(otherNumber.toPrecision(3))
+
+
+const oneNumber = 1000000
+// console.log(oneNumber.toLocaleString('en-IN'))
+
+
+
+
+// MATHS
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(45.8))
+// console.log(Math.ceil(45.1))
+// console.log(Math.floor(45.9))
+// console.log(Math.max(45,4,5,8,7))
+// console.log(Math.min(45,1,5,8,7))
+
+
+
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor((Math.random()*10)) + 1)
+
+
+const min = 20
+const max = 30
+ console.log(Math.floor(Math.random() * (max - min + 1))+min) 
+
+
+
---
+++ 01_basic/08_datesInJs.js
@@ -0,0 +1,10 @@
+// console.log(Date)
+
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+
+// let myCreatedDate = new Date(2025,0,4)
+let myCreatedDate = new Date(2025,0,4,45,4,5)
+console.log(myCreatedDate.toDateString())
