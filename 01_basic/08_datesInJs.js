40c5d118ead1fb8139251663c119b131aa3857c7
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-05 13:20:08 +0000

    date and time

--- 01_basic/08_datesInJs.js
+++ 01_basic/08_datesInJs.js
@@ -7,5 +7,22 @@ let myDate = new Date()
 
 // let myCreatedDate = new Date(2025,0,4)
 let myCreatedDate = new Date(2025,0,4,45,4,5)
-console.log(myCreatedDate.toDateString())
+// console.log(myCreatedDate.toDateString())
+
+let creatDate = new Date(2025,0,23,5,3)
+// console.log(creatDate.toLocaleString())
+
+
+// time stamp
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(creatDate.getTime())
+// console.log(creatDate.getTime())
+
+// how to get second
+
+console.log(Math.round(Date.now() / 1000))
+
+
 
