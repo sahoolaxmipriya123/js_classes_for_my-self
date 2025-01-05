e2f1085972d9e3b6f95525bb4a34803b9b326666
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-05 13:44:34 +0000

    arrys

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,19 @@
+// how to write array
+// in squre bracket []
+
+let arr = [0, 1, 2, 3, 4, ]
+
+
+// array methods
+
+// arr.push(5)  //add 5 in  last end.
+// arr.push(5)
+// arr.push(5)
+// arr.push(5)
+// arr.pop() //delete one element from last end.
+// arr.unshift(9)
+arr.shift()  //no need to put any value.
+
+
+console.log(arr)
+
