11c31a25b3cb82093044d284b557a3f660e31718
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-08 14:15:38 +0000

    function

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -35,4 +35,40 @@ function addTwoNumber(num1 , num2){
         }
         return `${username} just logged in`
     }
-     console.log(loggedIn("laxmi"))
+   //   console.log(loggedIn("laxmi"))
+
+
+     
+     function calculating (...num1){
+          return num1 
+     }
+   //   console.log(calculating(200,300,400,560))
+
+
+   //   how to pass object in function
+
+   let user = {
+      name : "laxmipriya",
+      price : 199
+   }
+
+
+     function handleObject(anyobject){
+      console.log(`the user name is ${anyobject.name} and he pay ${anyobject.price}`)
+     }
+   //   handleObject(user)
+
+   handleObject({
+      name : "priya",
+      price : 456
+   })
+
+   //how to pass array in function
+
+   let myNewArray = [456,458,789,852,]
+    
+   function handleArray (myArray){
+         return (myArray[2])
+   }
+
+console.log(handleArray(myNewArray))
