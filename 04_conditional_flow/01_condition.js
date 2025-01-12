1e50bfe475801c99aa7de6d25649930f90ce2e60
sahoolaxmipriya123 <151902241+sahoolaxmipriya123@users.noreply.github.com>
2025-01-12 04:23:13 +0000

    conditions

--- 04_conditional_flow/01_condition.js
+++ 04_conditional_flow/01_condition.js
@@ -1,7 +1,23 @@
 // if
 
-if (true) {
-    console.log("hello world");
+// if (2 != 3) {
+//     console.log("hello world");
     
+    
+// }
+// 3!=2
+// < , > , <= ,>= ,== ,===  ,!=
+
+
+// ifelse
+
+
+let temperature =42
+
+if (temperature <=40) {
+    console.log("its a normal temperature")
+} else {
+    console.log("temperature is high")
     
 }
+
